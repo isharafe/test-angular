8b5a84417d5a5df95a625c2044bb43349816f82a
Ruchira <ruchira@coredevone.com>
2022-07-09 10:17:08 +0530

    routers mapping

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "employees", component: EmployeesComponent},
+  {path: "employee/:id", component: EmployeeComponent},
+  {path: "", redirectTo: "/employees", pathMatch: "full"},
+  {path: "**", redirectTo: "/employees", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
